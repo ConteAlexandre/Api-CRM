e1d32f5e372dfa509185f34cf04ff09e18385f53
ConteAlexandre <pro.alexandre.conte@gmail.com>
2021-04-02 11:24:35 +0200

    Update Admin

--- src/Admin/ActionTypeAdmin.php
+++ src/Admin/ActionTypeAdmin.php
@@ -3,7 +3,9 @@
 namespace App\Admin;
 
 use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 
@@ -12,11 +14,48 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
  */
 class ActionTypeAdmin extends AbstractAdmin
 {
+    /**
+     * @param FormMapper $form
+     */
     protected function configureFormFields(FormMapper $form)
     {
         $form
             ->add('name', TextType::class)
             ->add('enabled', CheckboxType::class)
+        ;
+    }
+
+    /**
+     * @param ListMapper $list
+     */
+    protected function configureListFields(ListMapper $list)
+    {
+        $list
+            ->addIdentifier('name', null, [
+                'route' => [
+                    'name' => 'show',
+                ],
+            ])
+            ->add('enabled')
+            ->add('createdAt')
+            ->add('updatedAt')
+            ->add('createdBy')
+            ->add('updatedBy')
+        ;
+    }
+
+    /**
+     * @param ShowMapper $show
+     */
+    protected function configureShowFields(ShowMapper $show)
+    {
+        $show
+            ->add('name')
+            ->add('enabled')
+            ->add('createdAt')
+            ->add('updatedAt')
+            ->add('createdBy')
+            ->add('updatedBy')
         ;
     }
 }
--- src/Admin/ActionsAdmin.php
+++ src/Admin/ActionsAdmin.php
@@ -6,6 +6,7 @@ use Sonata\AdminBundle\Admin\AbstractAdmin;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Form\FormMapper;
 use Sonata\AdminBundle\Form\Type\ModelType;
+use Sonata\AdminBundle\Show\ShowMapper;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 
@@ -14,6 +15,9 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
  */
 class ActionsAdmin extends AbstractAdmin
 {
+    /**
+     * @param FormMapper $form
+     */
     protected function configureFormFields(FormMapper $form)
     {
         $form
@@ -40,6 +44,9 @@ class ActionsAdmin extends AbstractAdmin
         ;
     }
 
+    /**
+     * @param ListMapper $list
+     */
     protected function configureListFields(ListMapper $list)
     {
         $list
@@ -48,6 +55,27 @@ class ActionsAdmin extends AbstractAdmin
                     'name' => 'show',
                 ],
             ])
+            ->add('actionType')
+            ->add('description')
+            ->add('user')
+            ->add('client')
+            ->add('invoice')
+            ->add('devis')
+            ->add('exchange')
+            ->add('createdAt')
+            ->add('updatedAt')
+            ->add('createdBy')
+            ->add('updatedBy')
+        ;
+    }
+
+    /**
+     * @param ShowMapper $show
+     */
+    protected function configureShowFields(ShowMapper $show)
+    {
+        $show
+            ->add('title')
             ->add('actionType')
             ->add('description')
             ->add('user')
