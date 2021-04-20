e0a087fab2602096c7e35442c2e60b1099fbd116
ConteAlexandre <pro.alexandre.conte@gmail.com>
2021-04-20 11:59:30 +0200

    Update contreoller for action archived

--- src/Controller/User/UserActionController.php
+++ src/Controller/User/UserActionController.php
@@ -2,16 +2,35 @@
 
 namespace App\Controller\User;
 
+use App\Manager\ClientManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Finder\Exception\AccessDeniedException;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 /**
  * Class UserActionController
  */
 class UserActionController extends AbstractController
 {
-    public function archivedAction($slug)
+    /**
+     * @param               $slug
+     * @param ClientManager $clientManager
+     *
+     * @return RedirectResponse
+     */
+    public function archivedAction($slug, ClientManager $clientManager)
     {
         $user = $this->getUser();
-        $client =
+        $client = $clientManager->getClientBySlug($slug);
+
+        if ($user) {
+            $client->setIsArchived(true);
+
+            $this->addFlash('success', 'The client is archived');
+
+            return $this->redirectToRoute('list_client');
+        } else {
+            throw new AccessDeniedException('Not Authorized');
+        }
     }
 }
